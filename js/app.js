d9158c03e16a46fc28501664089daf4879d5ec9f
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-06-28 15:35:20 +0500

    Added showBalance

--- js/app.js
+++ js/app.js
@@ -17,13 +17,51 @@ class UI {
   }
 
   //submit budget form
-  submitBudgetForm (){
-    console.log('hello world!')
+  submitBudgetForm() {
     const value = this.budgetInput.value;
     if (value < 0 || value === "") {
       this.budgetFeedback.classList.add("showItem");
+      this.budgetFeedback.innerHTML =
+        "<p> Value cannot be empty or negative </p>";
+      const self = this;
+      setTimeout(function () {
+        self.budgetFeedback.classList.remove("showItem");
+      }, 3000);
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = "";
+      this.showBalance();
+    }
+  }
+  //Show Balance
+  showBalance() {
+    console.log("hey! What's up?");
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if (total > 0) {
+      this.balanceAmount.classList.remove("showRed", "showBlack");
+      this.balanceAmount.classList.add("showGreen");
+    } else if (total < 0) {
+      this.balanceAmount.classList.remove("showGreen", "showBlack");
+      this.balanceAmount.classList.add("showRed");
+    } else if (total === 0) {
+      this.balanceAmount.classList.remove("showGreen", "showRed");
+      this.balanceAmount.classList.add("showBlack");
+    }
+  }
+  //submit expense form
+  submitExpenseForm() {
+    const expenseValue = this.expenseAmount.value;
+    const amountValue = this.expenseAmount.value;
+    if (expenseValue ) {
       
     }
+  }
+  //total expense
+  totalExpense() {
+    let total = 400;
+    return total;
   }
 }
 
@@ -45,6 +83,7 @@ function eventListeners() {
   //expense form submit
   budgetForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    ui.submitExpenseForm();
   });
 
   //expense list click
@@ -53,4 +92,4 @@ function eventListeners() {
 
 document.addEventListener("DOMContentLoaded", function () {
   eventListeners();
-}); 
+});
